374d2b3b8c53ab9a7cf3768caaff325e547c0f67
Ojidaemo <vit.martsinovich@gmail.com>
2023-08-04 11:40:50 +0300

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Vitali Martsinovich on 2023-08-04.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
